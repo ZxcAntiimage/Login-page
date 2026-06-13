4503ec66f9b2b228e4b7addb0ddcd63e386d25b7
ZxcAntiimage <fasionergg@gmail.com>
2026-06-13 16:12:33 +0300

    fix: prisma.service

--- apps/backend/prisma/prisma.service.ts
+++ apps/backend/prisma/prisma.service.ts
@@ -4,19 +4,26 @@ import { Injectable, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "../src/generated/prisma/client";
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit{
-    constructor(){
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    constructor() {
         const adapter = new PrismaMariaDb({
-        host: process.env.DATABASE_HOST,
-        user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-        connectionLimit: 5,
-        ssl: {rejectUnauthorized: false},
-        connectTimeout: 10000,
-        acquireTimeout: 10000,
-        });
-        super({adapter})
+            host: process.env.DATABASE_HOST,
+            port: process.env.DATABASE_PORT ? Number(process.env.DATABASE_PORT) : 11294,
+            user: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
+            connectionLimit: 5,
+            useTextProtocol: true,
+            connectTimeout: 15000,
+            acquireTimeout: 15000,
+            ssl: { 
+                rejectUnauthorized: false 
+            },
+            
+            permitInsecurePlugin: true 
+        } as any);
+        
+        super({ adapter });
     }
 
     async onModuleInit() {
