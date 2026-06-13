e2390c36cd4e2ceda89941c7bcf7169723da6e01
ZxcAntiimage <fasionergg@gmail.com>
2026-06-13 15:34:27 +0300

    fix: prisma.service

--- apps/backend/prisma/prisma.service.ts
+++ apps/backend/prisma/prisma.service.ts
@@ -1,26 +1,18 @@
 import "dotenv/config";
-import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { Injectable } from "@nestjs/common";
 import { PrismaClient } from "../src/generated/prisma/client";
 
-
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-    constructor() {
-        super({
-            datasources: {
-                db: {
-                    url: process.env.DATABASE_URL,
-                },
-            },
-        } as any);
-    }
-
-    async onModuleInit() {
-        await this.$connect();
-    }
-
-    async onModuleDestroy() {
-        await this.$disconnect();
+export class PrismaService extends PrismaClient{
+    constructor(){
+        const adapter = new PrismaMariaDb({
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        connectionLimit: 5,
+        });
+        super({adapter})
     }
 }
