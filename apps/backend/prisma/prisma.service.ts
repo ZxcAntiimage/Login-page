ffb3816a25e7395df6afb27989270e47a565933a
ZxcAntiimage <fasionergg@gmail.com>
2026-06-13 15:11:24 +0300

    fix: адаптер prisma переведен на использование строки DATABASE_URL

--- apps/backend/prisma/prisma.service.ts
+++ apps/backend/prisma/prisma.service.ts
@@ -7,13 +7,7 @@ import { PrismaClient } from "../src/generated/prisma/client";
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
     constructor() {
-        const adapter = new PrismaMariaDb({
-            host: process.env.DATABASE_HOST,
-            user: process.env.DATABASE_USER,
-            password: process.env.DATABASE_PASSWORD,
-            database: process.env.DATABASE_NAME,
-            connectionLimit: 5,
-        });
+         const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
         super({ adapter });
     }
 
